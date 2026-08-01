ee194c7a85238d39f2749552c54fb0a6ff8b4d57
rajhodedara <68367766+rajhodedara@users.noreply.github.com>
2026-08-01 08:33:48 +0530

    fix: allow users to reconfigure from within stremio by adding /:config/configure route

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-app.get('/configure', (req, res) => {
+app.get(['/configure', '/:config/configure'], (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'configure.html'));
 });
 
