d691ea0e7e1bdbeb81cd7ef8a185e1ae11fcbcb2
rajhodedara <68367766+rajhodedara@users.noreply.github.com>
2026-08-16 20:05:06 +0530

    chore: setup Cloudflare worker deployment pipeline

---
+++ cloudflare-worker/index.js
@@ -0,0 +1,8 @@
+// TODO: PASTE YOUR CLOUDFLARE WORKER CODE HERE!
+// Go to your Cloudflare Dashboard, copy the code from your existing StreamFree proxy worker, and paste it here.
+
+export default {
+  async fetch(request, env, ctx) {
+    return new Response('Cloudflare Worker placeholder - Please paste your real code here!', { status: 200 });
+  },
+};
