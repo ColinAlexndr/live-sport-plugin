010b5e6148b04a8be0ffd3fea67877917f6370b0
rajhodedara <68367766+rajhodedara@users.noreply.github.com>
2026-08-23 20:48:45 +0530

    chore: remove scratch files

--- testDlhd.js
+++
@@ -1,6 +0,0 @@
-const BrowserSnifferService = require('./src/services/BrowserSnifferService');
-const sniffer = new BrowserSnifferService();
-sniffer.sniff('https://dlhd.so/embed/stream-78.php', { referer: 'https://dlhd.so/' })
-  .then(url => console.log('Found:', url))
-  .catch(console.error)
-  .finally(() => process.exit());
